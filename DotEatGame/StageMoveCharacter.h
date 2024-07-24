a7c981b4ede307eec60a612f2525c2d3bd457032
kmm-2459308 <kmm2459308@stu.o-hara.ac.jp>
2024-07-24 11:29:05 +0900

    DotEatGame

---
+++ DotEatGame/StageMoveCharacter.h
@@ -0,0 +1,13 @@
+//======================================
+//	ドットイートゲーム キャラクタ移動
+//======================================
+// ★ここにインクルードカード(開始)を記入してください
+#ifndef __STAGE_MOVE_CHARATER_H
+#define __STAGE_MOVE_CHARATER_H
+#include "Stage.h"
+
+void MovePlayer(Stage* stage);
+void MoveMonsters(Stage* stage);
+
+// ★ここにインクルードカード(終了)を記入してください
+#endif // !__STAGE_MOVE_CHARATER_H
